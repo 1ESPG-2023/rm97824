ba4f45664d5a4231d893188259de197bc581f849
ViniciusBarrosFiap <rm97824@fiap.com.br>
2023-04-18 10:32:20 -0300

    Usando o .map

--- js/zyb-5647.js
+++ js/zyb-5647.js
@@ -11,12 +11,21 @@ const liElement = document.getElementsByTagName("li");
 let num1 = [1,2,3,4,5];
 let num2 = [6,7,8,9,10];
 let num3 = [num1, num2];
+
 console.log(`Array 1: ${num1}`);
 console.log(`Array 2: ${num2}`);
 console.log(`Array 3: ${num3}`);
 
 //Listando o Array - Técnica especial
-num3.forEach((itemDoArray)=>{
-    console.log(`Array 3 sendo listado: ${itemDoArray}`)
+// num3.forEach((itemDoArray)=>{
+//     console.log(`Array 3 sendo listado: ${itemDoArray}`)
+// })
+
+//Listando o array com map
+num3.map((numero, key)=>{
+
+    console.log(`${(key + 1)} - Itens do array - ${numero}`);
+    numero.forEach((digitos)=>{
+        console.log(digitos)
+    })
 })
-console.log(`Array 3 sendo listado: ${num3}`)
